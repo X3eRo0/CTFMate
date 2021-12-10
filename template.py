6b78a23c5158160ae0040f617d55d1405d580a88
X3eRo0 <psinghania256@gmail.com>
2021-12-10 23:32:25 +0530

    fixed template

--- template.py
+++ template.py
@@ -11,13 +11,14 @@ import pwn
 
 
 # Set up pwntools for the correct architecture
-exe  = pwn.context.binary = pwn.ELF('%s')
+exe = pwn.context.binary = pwn.ELF('%s')
 pwn.context.terminal = ["tilix", "-t", "CTFMate", "-a", "session-add-right", "-e"]
 pwn.context.delete_corefiles = True
 pwn.context.rename_corefiles = False
 
 host = pwn.args.HOST or '%s'
 port = int(pwn.args.PORT or %s)
+
 
 def local(argv=[], *a, **kw):
     '''Execute the target binary locally'''
@@ -26,12 +27,14 @@ def local(argv=[], *a, **kw):
     else:
         return pwn.process([exe.path] + argv, *a, **kw)
 
+
 def remote(argv=[], *a, **kw):
     '''Connect to the process on the remote host'''
     io = pwn.connect(host, port)
     if pwn.args.GDB:
         pwn.gdb.attach(io, gdbscript=gdbscript)
     return io
+
 
 def start(argv=[], *a, **kw):
     '''Start the exploit against the target.'''
@@ -40,11 +43,13 @@ def start(argv=[], *a, **kw):
     else:
         return remote(argv, *a, **kw)
 
+
 gdbscript = '''
 continue
 '''.format(**locals())
 
 io = None
+
 
 def GetOffsetStdin():
     log_level = pwn.context.log_level
@@ -77,8 +82,8 @@ def GetOffsetArgv():
 
 io = start()
 
-#===========================================================
+# ===========================================================
 #                    EXPLOIT GOES HERE
-#===========================================================
+# ===========================================================
 
 io.interactive()
