1b4396927eaf914cd0c9e3d3971c8f7c56fdb00a
X3eRo0 <psinghania256@gmail.com>
2023-05-15 14:03:45 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install elfutils patchelf
+sudo apt-get install -y elfutils patchelf
 git clone https://github.com/X3eRo0/CTFMate.git ~/CTFMate
 python -m pip install -r ~/CTFMate/requirements.txt
 chmod +x ~/CTFMate/ctfmate.py
